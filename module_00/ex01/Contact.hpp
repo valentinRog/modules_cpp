7bddabc73c8bef83d9a51708550d290d0b81e5bd
valentinRog <valentin.rogister@gmail.com>
2022-05-09 12:05:21 +0200

    yo

---
+++ module_00/ex01/Contact.hpp
@@ -0,0 +1,18 @@
+#ifndef CONTACT_HPP
+#define CONTACT_HPP
+
+#include <string>
+
+class Contact {
+
+private:
+  std::string _fist_name;
+  std::string _last_name;
+  std::string _phone_number;
+  std::string _darkest_secret;
+
+public:
+  Contact();
+};
+
+#endif
