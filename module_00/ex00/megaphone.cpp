8310e60824460a1a9fe3f900701874ee02d4c233
valentinRog <valentin.rogister@gmail.com>
2022-05-09 10:50:07 +0200

    yo

--- module_00/ex00/megaphone.cpp
+++ module_00/ex00/megaphone.cpp
@@ -1,21 +1,19 @@
 #include <iostream>
 #include <string>
 
-# define DEFAULT_MSG "* LOUD AND UNBEARABLE FEEDBACK NOISE *"
+#define DEFAULT_MSG "* LOUD AND UNBEARABLE FEEDBACK NOISE *"
 
-int main(int argc, char **argv)
-{
-	std::string msg("");
+int main(int argc, char **argv) {
+  std::string msg("");
 
-	if (argc == 1)
-		msg = DEFAULT_MSG;
-	if (argc > 1)
-	{
-		for (int i = 1; i < argc; i++)
-			msg += argv[i];
-		for (std::string::iterator it = msg.begin(); it < msg.end(); it++)
-			*it = std::toupper(*it);
-	}
-	std::cout << msg << std::endl;
-	return 0;
+  if (argc == 1)
+    msg = DEFAULT_MSG;
+  if (argc > 1) {
+    for (int i = 1; i < argc; i++)
+      msg += argv[i];
+    for (std::string::iterator it = msg.begin(); it < msg.end(); it++)
+      *it = std::toupper(*it);
+  }
+  std::cout << msg << std::endl;
+  return 0;
 }
