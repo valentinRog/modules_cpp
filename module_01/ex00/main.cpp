be369b600cd35c5f5ab59f3c059066513f280113
valentinRog <valentin.rogister@gmail.com>
2022-05-13 21:43:56 +0200

    yo

--- module_01/ex00/main.cpp
+++ module_01/ex00/main.cpp
@@ -1,22 +1,17 @@
-#include <iostream>
 #include "Zombie.hpp"
-int	main( void )
-{
-	std::cout << "Creating the first Zombie, Robert, \"manually\"" << std::endl;
-	{
-		Zombie	robert("Robert");
-		robert.announce();
-	}
-	std::cout << "Creating the second Zombie, Georges, using the function"
-		" newZombie"<< std::endl;
-	{
-		Zombie	*georges = newZombie("Georges");
-		georges->announce();
-		delete georges;
-	}
-	{
-		std::cout << "Creating the third Zombie, Mark, using the function"
-			" randomChump"<< std::endl;
-		randomChump("Mark");
-	}
+
+int main(void) {
+  {
+    Zombie zombieOnStack("zombieOnStack");
+    zombieOnStack.announce();
+  }
+  std::cout << std::endl;
+  {
+    Zombie *zombieOnHeap = newZombie("zombieOnHeap");
+    zombieOnHeap->announce();
+    delete zombieOnHeap;
+  }
+  std::cout << std::endl;
+  { randomChump("randomZombie"); }
+  return 0;
 }
