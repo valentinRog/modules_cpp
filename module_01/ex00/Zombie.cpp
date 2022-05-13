c689006f970d715ae964764927ca35274bcb92f9
valentinRog <valentin.rogister@gmail.com>
2022-05-13 17:47:01 +0200

    yo

---
+++ module_01/ex00/Zombie.cpp
@@ -0,0 +1,9 @@
+#include "Zombie.hpp"
+
+Zombie::Zombie(std::string name) { _name = name; }
+
+Zombie::~Zombie() { std::cout << _name << " has been destroyed" << std::endl; }
+
+void Zombie::announce() {
+  std::cout << _name << ": BraiiiiiiinnnzzzZ..." << std::endl;
+}
---
+++ module_01/ex00/Zombie.hpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <string>
+
+class Zombie {
+
+private:
+  std::string _name;
+
+public:
+  Zombie(std::string name);
+  ~Zombie();
+  void announce();
+};
+
+Zombie *newZombie(std::string name);
+
+void randomChump(std::string name);
