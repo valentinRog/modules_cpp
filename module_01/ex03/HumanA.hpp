fa554c6bf27a854c2da18924a95fb9e1623a24ed
= <=>
2022-05-14 15:58:39 +0200

    yo

---
+++ module_01/ex03/HumanA.hpp
@@ -0,0 +1,19 @@
+#ifndef HUMAN_A_HPP
+#define HUMAN_A_HPP
+
+#include "Weapon.hpp"
+#include <iostream>
+#include <string>
+
+class HumanA {
+
+private:
+  std::string _name;
+  Weapon &_weapon;
+
+public:
+  HumanA(std::string name, Weapon &weapon);
+  void	attack();
+};
+
+#endif
---
+++ module_01/ex03/Weapon.hpp
@@ -0,0 +1,17 @@
+#ifndef WEAPON_HPP
+#define WEAPON_HPP
+
+#include <string>
+
+class Weapon {
+
+private:
+  std::string _type;
+
+public:
+  Weapon(std::string type);
+  std::string &getType();
+  void setType(std::string type);
+};
+
+#endif
