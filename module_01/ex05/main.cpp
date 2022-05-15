4ccfa909b0cb95a722d3a04d0f5d751bcefc0fa1
= <=>
2022-05-15 12:45:35 +0200

    yo

--- module_01/ex05/main.cpp
+++ module_01/ex05/main.cpp
@@ -1,15 +1,14 @@
 #include "Harl.hpp"
 
-int	main( void )
-{
-	Harl harl;
-	harl.complain("debug");
-	std::cout << std::endl;
-	harl.complain("info");
-	std::cout << std::endl;
-	harl.complain("warning");
-	std::cout << std::endl;
-	harl.complain("error");
-	std::cout << std::endl;
-	harl.complain("yo");
+int main(void) {
+  Harl harl;
+  harl.complain("debug");
+  std::cout << std::endl;
+  harl.complain("info");
+  std::cout << std::endl;
+  harl.complain("warning");
+  std::cout << std::endl;
+  harl.complain("error");
+  std::cout << std::endl;
+  harl.complain("yo");
 }
