38d72eba789a91c16afccf4e9790e07548400c2f
valentinRog <valentin.rogister@gmail.com>
2022-05-18 11:36:33 +0200

    yo

---
+++ module_02/ex01/Fixed.hpp
@@ -0,0 +1,32 @@
+#ifndef FIXED_HPP
+#define FIXED_HPP
+
+#include <cmath>
+#include <iostream>
+
+class Fixed {
+
+private:
+	int _raw;
+
+	static const int _fractionalBits;
+
+public:
+	Fixed();
+	Fixed( const Fixed &other );
+	Fixed( const int n );
+	Fixed( const float n );
+	Fixed &operator=( const Fixed &other );
+	~Fixed();
+
+	int	 getRawBits( void ) const;
+	void setRawBits( int const raw );
+
+	float toFloat() const;
+	int	  toInt() const;
+
+};
+
+std::ostream &operator<<( std::ostream &os, const Fixed &fixed );
+
+#endif
