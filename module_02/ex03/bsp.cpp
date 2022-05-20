ff06903bc440b49a56e0c0b8b630e7dc220b91a8
valentinRog <valentin.rogister@gmail.com>
2022-05-20 19:28:03 +0200

    yo

--- module_02/ex03/bsp.cpp
+++ module_02/ex03/bsp.cpp
@@ -3,22 +3,27 @@
 
 bool bsp( Point const a, Point const b, Point const c, Point const p ) {
 
-    float aX( a.getX().toFloat() );
-    float aY( a.getY().toFloat() );
-    float bX( b.getX().toFloat() );
-    float bY( b.getY().toFloat() );
-    float cX( c.getX().toFloat() );
-    float cY( c.getY().toFloat() );
-    float pX( p.getX().toFloat() );
-    float pY( p.getY().toFloat() );
+    Fixed aX( a.getX() );
+    Fixed aY( a.getY() );
+    Fixed bX( b.getX() );
+    Fixed bY( b.getY() );
+    Fixed cX( c.getX() );
+    Fixed cY( c.getY() );
+    Fixed pX( p.getX() );
+    Fixed pY( p.getY() );
 
-    float s1 = cY - aY;
-    float s2 = cX - aX;
-    float s3 = bY - aY;
-    float s4 = pY - aY;
+    Fixed s1 = cY - aY;
+    Fixed s2 = cX - aX;
+    Fixed s3 = bY - aY;
+    Fixed s4 = pY - aY;
 
-    float w1 = ( aX * s1 + s4 * s2 - pX * s1 ) / ( s3 * s2 - ( bX - aX ) * s1 );
-    float w2 = ( s4 - w1 * s3 ) / s1;
+    Fixed w1 = ( aX * s1 + s4 * s2 - pX * s1 ) / ( s3 * s2 - ( bX - aX ) * s1 );
+    Fixed w2 = ( s4 - w1 * s3 ) / s1;
+
+    std::cout << "w1=" << w1 << std::endl;
+    std::cout << "w2=" << w2 << std::endl;
+
+    std::cout << "sum=" << w1 + w2 << std::endl;
 
     return w1 > 0 && w2 > 0 && ( w1 + w2 ) < 1;
 }
