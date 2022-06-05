c76c42f4e09b2e4014f0ce48bfe5150861ca4d96
valentinRog <valentin.rogister@gmail.com>
2022-06-05 18:10:49 +0200

    yo

--- module_07/whatever.hpp
+++ module_07/whatever.hpp
@@ -7,10 +7,10 @@ template <typename T> void swap( T &a, T &b ) {
     b     = tmp;
 }
 
-template <typename T> const T &min( const T &a, const T &b ) {
+template <typename T> T const &min( T const &a, T const &b ) {
     return ( a < b ? a : b );
 }
-template <typename T> const T &max( const T &a, const T &b ) {
+template <typename T> T const &max( T const &a, T const &b ) {
     return ( a > b ? a : b );
 }
 
