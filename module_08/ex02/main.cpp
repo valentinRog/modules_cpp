6eaa3206a77d10556b26943b2b8b855c79c4f953
valentinRog <valentin.rogister@gmail.com>
2022-06-09 23:13:44 +0200

    yo

--- module_08/ex02/main.cpp
+++ module_08/ex02/main.cpp
@@ -1,33 +1,63 @@
 #include "MutantStack.hpp"
 #include <iostream>
+#include <algorithm>
+#include <array>
+#include <list>
+
+struct uniqueFunctor {
+
+public:
+    uniqueFunctor() : _i( 0 ) {}
+    int operator()() { return _i++; }
+
+private:
+    int _i;
+};
 
 int main() {
-    MutantStack<int> mstack;
+    std::array <int , 5> arr;
+    std::generate(arr.begin(), arr.end(), uniqueFunctor());
 
-    mstack.push( 5 );
-    mstack.push( 17 );
+    {
+        MutantStack<int> mstack;
 
-    std::cout << mstack.top() << std::endl;
-
-    mstack.pop();
-
-    std::cout << mstack.size() << std::endl;
-
-    mstack.push( 3 );
-    mstack.push( 5 );
-    mstack.push( 737 );
-    mstack.push( 0 );
-
-    MutantStack<int>::iterator it  = mstack.begin();
-    MutantStack<int>::iterator ite = mstack.end();
-
-    ++it;
-    --it;
-    while ( it != ite ) {
-        std::cout << *it << std::endl;
-        ++it;
+        for (std::array<int, 5>::iterator it = arr.begin(); it != arr.end(); it++) {
+            mstack.push(*it);
+        }
+        for (MutantStack<int>::iterator it = mstack.begin(); it != mstack.end(); it++) {
+            std::cout << *it << std::endl;
+        }
+        std::cout << std::endl;
+        for (MutantStack<int>::reverse_iterator it = mstack.rbegin(); it != mstack.rend(); it++) {
+            std::cout << *it << std::endl;
+        }
+        std::cout << std::endl;
+        mstack.pop();
+        mstack.pop();
+        for (MutantStack<int>::const_iterator it = mstack.begin(); it != mstack.end(); it++) {
+            std::cout << *it << std::endl;
+        }
     }
-    std::stack<int> s( mstack );
+    std::cout << "-----------------------------------------" << std::endl;
+    {
+        std::list<int> mstack;
 
+        for (std::array<int, 5>::iterator it = arr.begin(); it != arr.end(); it++) {
+            mstack.push_back(*it);
+        }
+        for (std::list<int>::iterator it = mstack.begin(); it != mstack.end(); it++) {
+            std::cout << *it << std::endl;
+        }
+        std::cout << std::endl;
+        for (std::list<int>::reverse_iterator it = mstack.rbegin(); it != mstack.rend(); it++) {
+            std::cout << *it << std::endl;
+        }
+        std::cout << std::endl;
+        mstack.pop_back();
+        mstack.pop_back();
+        for (std::list<int>::const_iterator it = mstack.begin(); it != mstack.end(); it++) {
+            std::cout << *it << std::endl;
+        }
+    }
     return 0;
 }
